f9f6a66040035649b684eb74376fb52ce11155e6
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-10-16 10:41:20 +0200

    Add basic form validation

--- script.js
+++ script.js
@@ -10,20 +10,52 @@ inputs.forEach(input => {
           errors[0].textContent = "";
         }
         else {
-          errors[0].textContent = "GIB EMAIL, SIR!";
+          errors[0].textContent = "Sir, please enter a valid E-Mail";
         }
         break;
       case "country":
+        if (input.validity.valid) {
+            errors[1].textContent = "";
+          }
+        else {
+          errors[1].textContent = "Sir, please fill the country field";
+        }
         break;
       case "zip-code":
-        break;
+        errors[2].textContent = checkZIP(input.value);
+        break;  
       case "password":
+        if (input.validity.valid) {
+          errors[3].textContent = "";
+        }
+        else {
+          errors[3].textContent = "Sir, please enter a password";
+        }
         break;
       case "confirmation":
+        if (input.validity.valid) {
+          errors[4].textContent = "";
+        }
+        else {
+          errors[4].textContent = "Sir, please enter a password confirmation";
+        }
         break;
     }
   })
 });
+
+function checkZIP(str) {
+  const constraints = "^(D-)?\\d{5}$";
+  const regExp = /^(D-)?\d{5}$/;
+
+  if (regExp.test(str)) {
+    return "";
+  }
+  else {
+    return "Sir, please enter a valid german ZIP-Code (D-XXXXX)"
+  }
+
+}
 
 
 
